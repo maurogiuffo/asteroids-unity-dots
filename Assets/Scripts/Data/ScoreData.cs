d7d00ed1a1c1173a154632338c1158417c812c97
Mauro Giuffo <maurogiuffo@gmail.com>
2022-04-13 01:42:47 -0300

    score system

---
+++ Assets/Scripts/Data/ScoreData.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+
+namespace Data
+{
+    [GenerateAuthoringComponent]
+    public struct ScoreData: IComponentData
+    {
+        public int value;
+    }
+}
---
+++ Assets/Scripts/Systems/ScoreSystem.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Unity.Entities;
+using Unity.Jobs;
+
+namespace Systems
+{
+    [UpdateBefore(typeof(DestroyDeadSystem))]
+    public class ScoreSystem : JobComponentSystem
+    {
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            RequireSingletonForUpdate<ScoreData>();
+            var source= GetSingletonEntity<ScoreData>();
+            var scoreData = EntityManager.GetComponentData<ScoreData>(source);
+
+            Entities
+                .WithAll<AsteroidTag>()
+                .ForEach((in HealthData healthData, in AsteroidTypeData typeData) =>
+                {
+                    if (!healthData.isDead) return;
+                    switch (typeData.type)
+                    {
+                        case 0: scoreData.value += 10; break;
+                        case 1: scoreData.value += 40; break;
+                        case 2: scoreData.value += 100; break;
+                    }
+                }).Run();
+
+            return default;
+        }
+    }
+}
